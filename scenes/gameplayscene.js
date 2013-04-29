f3d82915b5cb272e25bce48fb8fb1ff2eea5f78f
Nick Bernhardt <nicholasjbernhardt@gmail.com>
2013-04-28 19:32:09 -0500

    Updated Cascade and Collisions

--- scenes/gameplayscene.js
+++ scenes/gameplayscene.js
@@ -56,8 +56,8 @@ var GamePlayScene = function(game, canv)
     {
       if(cascade[cascadeIndex*(i+1)].y == 0)
         cascade[cascadeIndex*(i+1)].x = Math.floor(Math.random()*16);
-      if(cascade[cascadeIndex*(i+1)].solid) canv.context.fillRect(cascade[cascadeIndex*(i+1)].x*40,cascade[cascadeIndex*(i+1)].y*40,40,40);
-      else                                  canv.context.strokeRect(cascade[cascadeIndex*(i+1)].x*40,cascade[cascadeIndex*(i+1)].y*40,40,40);
+      if(cascade[cascadeIndex*(i+1)].solid) canv.context.fillRect(cascade[cascadeIndex*(i+1)].x*80,cascade[cascadeIndex*(i+1)].y*80,80,80);
+      else                                  canv.context.strokeRect(cascade[cascadeIndex*(i+1)].x*80,cascade[cascadeIndex*(i+1)].y*80,80,80);
       cascade[cascadeIndex*(i+1)].y = (cascade[cascadeIndex*(i+1)].y+1)%8
       cascadeIndex = (cascadeIndex+1)%20;
     }
@@ -68,25 +68,25 @@ var GamePlayScene = function(game, canv)
     //Detect collisions
     var osx;
     var obx;
-    if     (p1.xvel > 0) { osx = p1.x        -5; obx = p1.x+p1.xvel+5; }
-    else if(p1.xvel < 0) { osx = p1.x+p1.xvel-5; obx = p1.x        +5; }
-    else                 { osx = p1.x        -5; obx = p1.x        +5; }
+    if     (p1.xvel > 0) { osx = p1.x        -20; obx = p1.x+p1.xvel+20; }
+    else if(p1.xvel < 0) { osx = p1.x+p1.xvel-20; obx = p1.x        +20; }
+    else                 { osx = p1.x        -20; obx = p1.x        +20; }
     var osy;
     var oby;
-    if     (p1.yvel > 0) { osy = p1.y        -5; oby = p1.y+p1.yvel+5  }
-    else if(p1.yvel < 0) { osy = p1.y+p1.yvel-5; oby = p1.y        +5; }
-    else                 { osy = p1.y        -5; oby = p1.y        +5; }
+    if     (p1.yvel > 0) { osy = p1.y        -20; oby = p1.y+p1.yvel+20  }
+    else if(p1.yvel < 0) { osy = p1.y+p1.yvel-20; oby = p1.y        +20; }
+    else                 { osy = p1.y        -20; oby = p1.y        +20; }
 
     var tsx;
     var tbx;
-    if     (p2.xvel > 0) { tsx = p2.x        -5; tbx = p2.x+p2.xvel+5; }
-    else if(p2.xvel < 0) { tsx = p2.x+p2.xvel-5; tbx = p2.x        +5; }
-    else                 { tsx = p2.x        -5; tbx = p2.x        +5; }
+    if     (p2.xvel > 0) { tsx = p2.x        -20; tbx = p2.x+p2.xvel+20; }
+    else if(p2.xvel < 0) { tsx = p2.x+p2.xvel-20; tbx = p2.x        +20; }
+    else                 { tsx = p2.x        -20; tbx = p2.x        +20; }
     var tsy;
     var tby;
-    if     (p2.yvel > 0) { tsy = p2.y        -5; tby = p2.y+p2.yvel+5; }
-    else if(p2.yvel < 0) { tsy = p2.y+p2.yvel-5; tby = p2.y        +5; }
-    else                 { tsy = p2.y        -5; tby = p2.y        +5; }
+    if     (p2.yvel > 0) { tsy = p2.y        -20; tby = p2.y+p2.yvel+20; }
+    else if(p2.yvel < 0) { tsy = p2.y+p2.yvel-20; tby = p2.y        +20; }
+    else                 { tsy = p2.y        -20; tby = p2.y        +20; }
 
     var oyint = p1.y-((p1.x/p1.xvel)*p1.yvel);
     var tyint = p2.y-((p2.x/p2.xvel)*p2.yvel);
@@ -97,7 +97,7 @@ var GamePlayScene = function(game, canv)
     var yint = (oslope*xint)+oyint;
     if(p1.xvel == 0) { xint = p1.x; yint = tslope*xint + tyint; }
     if(p2.xvel == 0) { xint = p2.x; yint = oslope*xint + oyint; }
-    if(oslope == tslope) { xint = p1.x+(p1.xvel/2); yint = p1.y+(p1.yvel/2); }
+    if(oslope == tslope) { xint = p1.x+(p1.xvel/4); yint = p1.y+(p1.yvel/4); }
     
    /*//useful debugging
     if(Math.abs(p1.x-p2.x) < 10 && Math.abs(p1.y-p2.y) < 10)
@@ -116,10 +116,10 @@ var GamePlayScene = function(game, canv)
       //console.log('collide- '+xint+','+yint);
       hit[hiti].play();
       hiti = (hiti+1)%10;
-      if(p1.x > p2.x) { p1.x = xint+5; p2.x = xint-5; }
-      else            { p1.x = xint-5; p2.x = xint+5; }
-      if(p1.y > p2.y) { p1.y = yint+5; p2.y = yint-5; }
-      else            { p1.y = yint-5; p2.y = yint+5; }
+      if(p1.x > p2.x) { p1.x = xint+20; p2.x = xint-20; }
+      else            { p1.x = xint-20; p2.x = xint+20; }
+      if(p1.y > p2.y) { p1.y = yint+20; p2.y = yint-20; }
+      else            { p1.y = yint-20; p2.y = yint+20; }
 
       p1.score += Math.round(Math.abs(p1.xvel)+Math.abs(p1.yvel));
       p2.score += Math.round(Math.abs(p2.xvel)+Math.abs(p2.yvel));
